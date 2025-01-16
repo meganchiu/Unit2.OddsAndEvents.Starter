50061962ae768cbc0d022842162a0e714380d50a
Megan Chiu <megan.chiu.2014@gmail.com>
2025-01-15 21:31:08 -0800

    complete render numbers to number bank functionality

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-// TODO: this file! :)
+// Define initial array of 
+let numArr = [];
+
+const form = document.querySelector('form');
+form.addEventListener('submit', addNumber);
+
+// Add the submitted number into 'numArr', clear the form, and then rerender.
+function addNumber(event) {
+  event.preventDefault();
+
+  const num = Number(event.target.number.value);
+  numArr.push(num);
+  // console.log(numArr);
+
+  // Reset textbox to empty after submission
+  event.target.number.value = "";
+
+  // Re render after submission
+  render();
+}
+
+// Render numbers to the numbers bank
+function renderNumbersToNumbersBank() {
+  const numbersElements = numArr.map((number) => {
+    const numbersElement = document.createElement('span');
+    numbersElement.textContent = number;
+    numbersElement.classList = 'numbers';
+
+    return numbersElement;
+  })
+  const numbersBank = document.querySelector('#numberBank output');
+  numbersBank.replaceChildren(...numbersElements);
+}
+
+// Render numbers to the odd section
+function renderNumbersToOddSection() {
+
+}
+
+// Render numbers to the even bank
+function renderNumbersToEvenSection() {
+  
+}
+
+function render() {
+  renderNumbersToNumbersBank();
+  renderNumbersToOddSection();
+  renderNumbersToEvenSection();
+}
